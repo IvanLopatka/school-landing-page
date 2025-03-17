e58d67a2c25aecb1c0768349da75dee24400b667
Ivan Lopatka <ivan.lopatka630@gmail.com>
2025-03-17 13:47:26 +0200

    feat: add search icon & button, login/register button to navigator bar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,21 +4,35 @@ import { FC } from "react";
 
 export const NavigationBar: FC = () => {
   return (
-    <div className="flex items-center ">
+    <div className="flex items-center gap-x-8 p-4">
       <Image
         src="/WiSchool.svg"
         alt="Next.js logo"
         width={132}
         height={31}
         priority
-        className="mr-8"
+        className="mr-20"
       />
-      <div className="flex gap-x-8">
+      <div className="flex gap-x-8 mr-30">
         <Link href="#">Home</Link>
         <Link href="#">Courses</Link>
         <Link href="#">Instructors</Link>
         <Link href="#">Schedules</Link>
         <Link href="#">Contact Us</Link>
+      </div>
+      <div className="flex gap-[10px] font-sourceSansPro">
+        <button>
+          <Image src="/bx_search.svg" alt="Search" width={24} height={24} />
+        </button>
+        <button style={{ width: 75, height: 36 }} className="text-green-600">
+          Login
+        </button>
+        <button
+          style={{ width: 75, height: 36 }}
+          className="bg-green-500 text-white text-[16px] rounded-[5px] text-base px-[8px]"
+        >
+          Register
+        </button>
       </div>
     </div>
   );
