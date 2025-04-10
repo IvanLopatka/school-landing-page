e41f793ac1cb0ab717c9b7a8ce1a9b676af5b65a
Ivan Lopatka <ivan.lopatka630@gmail.com>
2025-04-10 09:17:15 +0300

    fix: add gap and margin to NavigationBar

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,7 +4,7 @@ import { FC } from "react";
 
 export const NavigationBar: FC = () => {
   return (
-    <nav className="max-w-[1440px] container-lg flex items-center gap-x-8 p-4">
+    <nav className="max-w-[1440px] container-lg flex items-center gap-x-24 p-4">
       <button className="max-[960px]:block mr-4 min-[960px]:hidden">
         <Image src="/Vector.svg" alt="menu-hamburger" width={21} height={16} />
       </button>
@@ -16,14 +16,14 @@ export const NavigationBar: FC = () => {
         priority
         className="mr-20"
       />
-      <div className="flex gap-x-8 mr-30 max-[960px]:hidden max-[1300px]:mr-10">
+      <div className="flex gap-x-8 mr-10 ml-10 max-[960px]:hidden max-[1300px]:mr-10">
         <Link href="#">Home</Link>
         <Link href="#">Courses</Link>
         <Link href="#">Instructors</Link>
         <Link href="#">Schedules</Link>
         <Link href="#">Contact Us</Link>
       </div>
-      <div className="flex gap-[10px] font-sourceSansPro">
+      <div className="flex gap-[10px] ml-30 font-sourceSansPro">
         <button className="max-[960px]:hidden mr-4">
           <Image src="/bx_search.svg" alt="Search" width={24} height={24} />
         </button>
