8d10e6e23838523203c1c0d3f841bf9c0126cccb
Ivan Lopatka <ivan.lopatka630@gmail.com>
2025-04-19 14:45:49 +0300

    feat: add meta-tag & prettier

--- src/components/FirstFrame.tsx
+++ src/components/FirstFrame.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 export const FirstFrame: React.FC = () => {
   return (
-    <div className=" max-w-[1240px] max-h-[605px] flex flex-wrap w-full h-full justify-end relative max max-[600px]:max-w-[485px] ">
-      <div className="z-2 relative flex justify-end items-center mr-1 h-[500px] mt-[60px] max-[1000px]:mt-[600px] max-[1000px]:absolute max-[1000px]:left-40 min-w-[401px] min-h-[400px]">
+    <div className="relative flex h-full max-h-[605px] w-full max-w-[1240px] flex-wrap justify-end max-[600px]:max-w-[485px]">
+      <div className="relative z-2 mt-[60px] mr-1 flex h-[500px] min-h-[400px] min-w-[401px] items-center justify-end max-[1000px]:absolute max-[1000px]:left-40 max-[1000px]:mt-[600px]">
         <Image
           src="/i-m-prepared-exam.png"
           alt="I'm prepared for exam"
           width={431}
           height={508}
-          className="z-2 absolute min-w-[431px] min-h-[508px] rounded-tl-[40px] rounded-bl-[40px] rounded-tr-[40px] rounded-br-[100px]"
+          className="absolute z-2 min-h-[508px] min-w-[431px] rounded-tl-[40px] rounded-tr-[40px] rounded-br-[100px] rounded-bl-[40px]"
         />
-        <div className="rounded-[20px] flex flex-col justify-center items-center mt-20 bg-white absolute right-80 z-10 w-[262px] h-[187px]">
-          <p
-            className="font-semibold w-[214px] h-[100px] Popins mb-5 pt-[10px] text-[20px]
-          "
-          >
-            Learn from best <p className="text-green-600">instructors</p> around
-            the globe
+        <div className="absolute right-80 z-10 mt-20 flex h-[187px] w-[262px] flex-col items-center justify-center rounded-[20px] bg-white">
+          <p className="Popins mb-5 h-[100px] w-[214px] pt-[10px] text-[20px] font-semibold">
+            Learn from best{' '}
+            <span className="text-green-600">
+              instructors
+            </span>{' '}
+            around the globe
           </p>
           <Image
             src="/five_mens-icon.png"
@@ -28,31 +28,34 @@ export const FirstFrame: React.FC = () => {
           />
         </div>
 
-        <div className="flex flex-col o absolute z-10 right-[-20px] mt-75 w-[129px] h-[146px] bg-white text-center rounded-tl-[6.89px] rounded-tr-[6.89px] rounded-br-[17.22px] rounded-bl-[6.89px] ">
+        <div className="absolute right-[-20px] z-10 mt-75 flex h-[146px] w-[129px] flex-col rounded-tl-[6.89px] rounded-tr-[6.89px] rounded-br-[17.22px] rounded-bl-[6.89px] bg-white text-center">
           <Image
             src="/FirstFrame-Icon.svg"
             alt="Icon"
-            className="ml-12 mt-4 "
+            className="mt-4 ml-12"
             width={30}
             height={30}
           />
-          <p className="text-[24px] font-bold text-green-600">15k</p>
-          <p className="text-[12px] text-gray-600 text-sm">
+          <p className="text-[24px] font-bold text-green-600">
+            15k
+          </p>
+          <p className="text-sm text-[12px] text-gray-600">
             Amazing students around the globe
           </p>
         </div>
-        <div className="bg-bg-img mt-[110px] w-[107px] h-[605px] left-[30px] flex relative z-[-1] items-center -top-15 min-[1000px]:rounded-tr-[30px] min-[1000px]:rounded-br-[30px] max-[1000px]:h-[1011px] max-[1000px]:w-[107px] max-[1000px]:-top-90"></div>
+        <div className="bg-bg-img relative -top-15 left-[30px] z-[-1] mt-[110px] flex h-[605px] w-[107px] items-center max-[1000px]:-top-90 max-[1000px]:h-[1011px] max-[1000px]:w-[107px] min-[1000px]:rounded-tr-[30px] min-[1000px]:rounded-br-[30px]"></div>
       </div>
 
-      <div className="z-3 absolute top-[120px] left-[40px] w-[589px] h-[270px] max-[600px]:left-0 flex flex-col">
-        <p className="popins font-[700] text-[60px]">
-          Quality <span className=" text-green-600">Education</span> By Any
-          Means
+      <div className="absolute top-[120px] left-[40px] z-3 flex h-[270px] w-[589px] flex-col max-[600px]:left-0">
+        <p className="popins text-[60px] font-[700]">
+          Quality{' '}
+          <span className="text-green-600">Education</span>{' '}
+          By Any Means
           <p>Necessary.</p>
         </p>
       </div>
-      <div className="bg-transparent absolute left-[30px] top-[300px] w-[213px] h-[213px] shadow-black-50 "></div>
-      <button className="w-[161px] h-[60px] absolute left-[40px] top-[400px] bg-button rounded-[5px]">
+      <div className="shadow-black-50 absolute top-[300px] left-[30px] h-[213px] w-[213px] bg-transparent"></div>
+      <button className="bg-button absolute top-[400px] left-[40px] h-[60px] w-[161px] rounded-[5px]">
         <p className="text-white">Get Started</p>
       </button>
     </div>
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
-import Link from "next/link";
-import { FC } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
 
 export const NavigationBar: FC = () => {
   return (
-    <nav className="max-w-[1440px] container-lg flex items-center gap-x-24 max-[700px]:gap-x-6 max-[700px]:ml-10 p-4">
-      <button className="max-[960px]:block mr-1 min-[960px]:hidden shrink-0">
-        <Image src="/Vector.svg" alt="menu-hamburger" width={21} height={16} />
+    <nav className="flex max-w-[1440px] items-center gap-x-24 bg-fuchsia-400 p-4 max-[700px]:ml-10 max-[700px]:gap-x-6">
+      <button className="mr-1 shrink-0 max-[960px]:block min-[960px]:hidden">
+        <Image
+          src="/Vector.svg"
+          alt="menu-hamburger"
+          width={21}
+          height={16}
+        />
       </button>
       <Image
         src="/WiSchool.svg"
@@ -14,26 +19,30 @@ export const NavigationBar: FC = () => {
         width={132}
         height={31}
         priority
-        className="mr-20 max-[700px]:mr-10"
       />
-      <div className="flex gap-x-8 mr-10 ml-10 max-[960px]:hidden max-[1300px]:mr-10">
+      <div className="flex gap-x-8 bg-amber-400 max-[960px]:hidden">
         <Link href="#">Home</Link>
         <Link href="#">Courses</Link>
         <Link href="#">Instructors</Link>
         <Link href="#">Schedules</Link>
         <Link href="#">Contact Us</Link>
       </div>
-      <div className="flex gap-[10px] ml-30 font-sourceSansPro">
-        <button className="max-[960px]:hidden mr-4">
-          <Image src="/bx_search.svg" alt="Search" width={24} height={24} />
+      <div className="font-sourceSansPro flex gap-[10px] bg-green-400">
+        <button className="max-[960px]:hidden">
+          <Image
+            src="/bx_search.svg"
+            alt="Search"
+            width={24}
+            height={24}
+          />
         </button>
-        <div className="flex gap-[10px] max-[1150px]:hidden">
+        <div className="ml-4 flex gap-[10px] max-[1150px]:hidden">
           <button className="text-button">Login</button>
-          <button className="bg-button text-white text-[16px] rounded-[5px] text-base px-[16px] py-[8px]">
+          <button className="bg-button rounded-[5px] px-[16px] py-[8px] text-base text-[16px] text-white">
             Register
           </button>
         </div>
-        <button className="min-[960px]:hidden flex items-center shrink-0 right-1 ml-30">
+        <button className="right-1 ml-30 flex shrink-0 items-center min-[960px]:hidden">
           <Image
             src="/carbon_user-avatar.svg"
             alt="cart-icon"
