f35c07b55e647ac08711f78a4e95359d37e4e29c
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-06-11 11:21:15 +0300

    feat: add ChooseCategory component with carousel functionality and custom arrows; update images and remove Programming.png

---
+++ src/components/ChooseCategory.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import React from 'react';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+import { CustomCarouselArrow } from './CustomCarouselArrow';
+
+import { FC } from 'react';
+import { FavoriteCourses } from './FavoriteCourses';
+
+const categories = [
+  {
+    iconTitle: 'Marketing',
+    iconUrl: '/marketing.png',
+    iconAlt: 'Learn marketing',
+  },
+  {
+    iconTitle: 'Design',
+    iconUrl: '/design.png',
+    iconAlt: 'laptop',
+  },
+  {
+    iconTitle: 'Programming',
+    iconUrl: '/programming.png',
+    iconAlt: 'programming',
+  },
+  {
+    iconTitle: 'Technology',
+    iconUrl: '/technology.png',
+    iconAlt: 'Technology',
+  },
+];
+const responsive = {
+  lg: {
+    breakpoint: { max: 4000, min: 769 },
+    items: 4,
+    slidesToSlide: 1,
+  },
+  md: {
+    breakpoint: { max: 768, min: 640 },
+    items: 3,
+    slidesToSlide: 1,
+  },
+  sm: {
+    breakpoint: { max: 639, min: 1 },
+    items: 2,
+    slidesToSlide: 1,
+  },
+};
+
+export const ChooseCategory: FC = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="flex justify-between px-4">
+        <h1 className="Poppins text-xl font-semibold text-wrap sm:text-2xl md:text-3xl">
+          Choose favourite course from top cartegories
+        </h1>
+        <h2 className="Poppins ml-5 text-lg font-normal text-nowrap sm:text-xl md:text-2xl">
+          See all
+        </h2>
+      </div>
+      <div>
+        <Carousel
+          itemClass="px-3"
+          arrows
+          infinite
+          className="mx-auto max-w-screen"
+          responsive={responsive}
+          autoPlay={false}
+          sliderClass="h-[50%] md:w-max"
+          keyBoardControl={true}
+          transitionDuration={50}
+          partialVisible={true}
+          customLeftArrow={
+            <CustomCarouselArrow direction="left" />
+          }
+          customRightArrow={
+            <CustomCarouselArrow direction="right" />
+          }
+        >
+          {categories.map((categorie, index) => (
+            <div
+              className="h-full rounded-xl py-5"
+              key={index}
+            >
+              <FavoriteCourses
+                iconAlt={categorie.iconAlt}
+                iconTitle={categorie.iconTitle}
+                iconUrl={categorie.iconUrl}
+              />
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/CustomCarouselArrow.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { FC } from 'react';
+import Image from 'next/image';
+import { twMerge } from 'tailwind-merge';
+interface CustomCarouselArrowProps {
+  onClick: () => void;
+  direction: 'left' | 'right';
+}
+
+export const CustomCarouselArrow: FC<
+  CustomCarouselArrowProps
+> = (props) => {
+  console.log('props', props);
+  return (
+    <button
+      onClick={props.onClick}
+      className={twMerge(
+        'absolute z-30 flex h-15 w-15 justify-center rounded-full bg-white active:bg-gray-200',
+        props.direction === 'left' ? 'left-6' : 'right-6'
+      )}
+    >
+      <Image
+        src="/right-arrow.svg"
+        alt="right arrow"
+        height={30}
+        width={30}
+        className={
+          props.direction === 'left'
+            ? 'rotate-180'
+            : undefined
+        }
+      />
+    </button>
+  );
+};
