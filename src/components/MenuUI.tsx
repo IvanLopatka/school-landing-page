5587fa8d20d76c9808a3aa3fdbf5fd41f6e5253d
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-07-25 10:25:53 +0300

    feat: enhance navigation with new MenuUI component and integrate it into NavigationBar; update drawer component for better mobile experience

--- src/components/MenuUI.tsx
+++ src/components/MenuUI.tsx
@@ -1,2 +1,56 @@
 import React from 'react';
 import { FC } from 'react';
+import Link from 'next/link';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+
+export const MenuUI: FC = () => {
+  return (
+    <div className="flex h-full w-full grow flex-col rounded-[16px] bg-zinc-50 p-5">
+      <div className="mx-auto max-w-md">
+        <DrawerDescription className="mb-2 text-zinc-600">
+          <div className="flex flex-col">
+            <Link
+              className="text-button text-2xl text-nowrap"
+              href="#"
+            >
+              Home
+            </Link>
+            <Link
+              className="text-button text-2xl text-nowrap"
+              href="#"
+            >
+              Courses
+            </Link>
+            <Link
+              className="text-button text-2xl text-nowrap"
+              href="#"
+            >
+              Instructors
+            </Link>
+            <Link
+              className="text-button text-2xl text-nowrap"
+              href="#"
+            >
+              Schedules
+            </Link>
+            <Link
+              className="text-button text-2xl text-nowrap"
+              href="#"
+            >
+              Contact Us
+            </Link>
+          </div>
+        </DrawerDescription>
+      </div>
+    </div>
+  );
+};
--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { FC } from 'react';
 import React from 'react';
 import { MenuUI } from './MenuUI';
-import { Button } from "@/components/ui/button"
+import { Button } from '@/components/ui/button';
 
 import {
   Drawer,
@@ -19,9 +19,9 @@ import {
 
 export const NavigationBar: FC = () => {
   return (
-    <Drawer direction="left">
-      <nav className="fixed top-0 right-0 left-0 z-20 mx-auto mb-10 flex items-center justify-between bg-white px-4 py-4 drop-shadow-md sm:static sm:container sm:mb-15 sm:px-0 sm:drop-shadow-none">
-        <div className="flex w-full items-center justify-between gap-x-6 sm:w-auto sm:gap-x-24">
+    <nav className="fixed top-0 right-0 left-0 z-20 mx-auto mb-10 flex items-center justify-between bg-white px-4 py-4 drop-shadow-md sm:static sm:container sm:mb-15 sm:px-0 sm:drop-shadow-none">
+      <div className="flex w-full items-center justify-between gap-x-6 sm:w-auto sm:gap-x-24">
+        <Drawer direction="left">
           <DrawerTrigger>
             <button className="sm:hidden">
               <Image
@@ -50,43 +50,43 @@ export const NavigationBar: FC = () => {
               height={32}
             />
           </button>
-        </div>
+        </Drawer>
+      </div>
 
-        <div className="hidden gap-x-3 sm:flex md:gap-x-8">
-          <Link className="text-nowrap" href="#">
-            Home
-          </Link>
-          <Link className="text-nowrap" href="#">
-            Courses
-          </Link>
-          <Link className="text-nowrap" href="#">
-            Instructors
-          </Link>
-          <Link className="text-nowrap" href="#">
-            Schedules
-          </Link>
-          <Link className="text-nowrap" href="#">
-            Contact Us
-          </Link>
-        </div>
+      <div className="hidden gap-x-3 sm:flex md:gap-x-8">
+        <Link className="text-nowrap" href="#">
+          Home
+        </Link>
+        <Link className="text-nowrap" href="#">
+          Courses
+        </Link>
+        <Link className="text-nowrap" href="#">
+          Instructors
+        </Link>
+        <Link className="text-nowrap" href="#">
+          Schedules
+        </Link>
+        <Link className="text-nowrap" href="#">
+          Contact Us
+        </Link>
+      </div>
 
-        <div className="font-sourceSansPro flex items-center gap-[10px]">
-          <button className="hidden sm:block">
-            <Image
-              src="/bx_search.svg"
-              alt="Search"
-              width={24}
-              height={24}
-            />
+      <div className="font-sourceSansPro flex items-center gap-[10px]">
+        <button className="hidden sm:block">
+          <Image
+            src="/bx_search.svg"
+            alt="Search"
+            width={24}
+            height={24}
+          />
+        </button>
+        <div className="hidden gap-[10px] lg:flex">
+          <button className="text-button">Login</button>
+          <button className="bg-button rounded-[5px] px-[16px] py-[8px] text-base text-[16px] text-white">
+            Register
           </button>
-          <div className="hidden gap-[10px] lg:flex">
-            <button className="text-button">Login</button>
-            <button className="bg-button rounded-[5px] px-[16px] py-[8px] text-base text-[16px] text-white">
-              Register
-            </button>
-          </div>
         </div>
-      </nav>
-    </Drawer>
+      </div>
+    </nav>
   );
 };
