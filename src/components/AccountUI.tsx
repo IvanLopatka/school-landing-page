f82606d031dd7bba2cb056096bb1fd05bfbb9586
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-07-26 10:38:39 +0300

    feat: add Popover component and related subcomponents for enhanced UI interactions

---
+++ src/components/AccountUI.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FC } from 'react';
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
+export const AccountUI: FC = () => {
+  return <div></div>;
+};
