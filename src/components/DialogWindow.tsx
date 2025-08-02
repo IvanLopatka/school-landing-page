08a304d454867fdc505aef4d19a4d6bf6a45daa9
Lopatka Ivan <lopatkaivan630@gmail.com>
2025-08-02 11:12:52 +0300

    Feat: add search input to dialog window

--- src/components/DialogWindow.tsx
+++ src/components/DialogWindow.tsx
@@ -3,12 +3,19 @@ import { FC } from 'react';
 
 import { DialogContent } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
 
 export const DialogWindow: FC = () => {
   return (
     <DialogContent showCloseButton={false}>
       <div>
-        <Input placeholder="Search" />
+        <Input placeholder="What do you want to know?" />
+        <Button
+          type="reset"
+          className="absolute top-6 right-5 shrink-0 border-none bg-transparent !shadow-none hover:bg-transparent"
+        >
+          <p className="text-black">X</p>
+        </Button>
       </div>
     </DialogContent>
   );
